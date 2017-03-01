60013bd4d504611c30d3310003fa074eab95c75d
Dolan <dolan_miu@hotmail.com>
2017-03-01 14:33:27 +0000

    web pack commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './sample.component';
+export * from './color-picker.component';
+export * from './sample.directive';
+export * from './sample.pipe';
+export * from './sample.service';
+export * from './color-picker.module';
+
